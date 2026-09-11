6cfbe9572b4f88cc4c454ff07b55b835115f57d1
Nico25dayo <3ryofukuchi0714@gmail.com>
2026-09-11 11:35:33 +0900

    Update pokemon-images.js

--- pokemon-images.js
+++ pokemon-images.js
@@ -1,1 +1,12 @@
+const pokemonImages = {
+  "フシギバナ": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+  "リザードン": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+  "カメックス": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+  "ゲンガー": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png",
+  "カビゴン": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/143.png",
+  "カイリュー": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png"
+};
 
+function getPokemonImage(name) {
+  return pokemonImages[name] || "";
+}
